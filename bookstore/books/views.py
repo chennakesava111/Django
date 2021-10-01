d76aa06cf211c5ff81229fbee5ce7f8b6abf31c6
Chennakesava Reddy Vemireddy <cv20264@corp.ojas-it.com>
2021-10-01 19:19:47 +0530

    Changes are made

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,22 +1,36 @@
 from books.models import Book, Review
 from django.shortcuts import redirect, render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    dbData = Book.objects.all()
-    context = {'books':dbData}
-    return render(request, 'index.html', context)
 
-def show(request,id):
-    singleBook = get_object_or_404(Book,pk=id)
-    reviews = Review.objects.all()
-    context = {'book':singleBook,'reviews':reviews}
-    return render(request, 'show.html', context)
+class BookListView(ListView):
 
-def review(request):
-    body = request.POST['review']
-    newReview = Review(body=body)
-    newReview.save()
-    return redirect('/book')
+    def get_queryset(self):
+        return Book.objects.all()
 
-def fun1(request):
+
+class BookDetailView(DetailView):
+    model = Book
+
+
+
+
+
+# def index(request):
+#     dbData = Book.objects.all()
+#     context = {'books':dbData}
+#     return render(request, 'index.html', context)
+
+# def show(request,id):
+#     singleBook = get_object_or_404(Book,pk=id)
+#     reviews = Review.objects.filter(book_id=id).order_by('-created_at')
+#     context = {'book':singleBook,'reviews':reviews}
+#     return render(request, 'show.html', context)
+
+# def review(request, id):
+#     body = request.POST['review']
+#     newReview = Review(body=body, book_id=id)
+#     newReview.save()
+#     return redirect('/book')
+
     
